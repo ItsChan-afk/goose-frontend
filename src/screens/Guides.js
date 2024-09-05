82a679010c85891ee671ecc8edaa5510da37653c
Mac <chanminswe26@gmail.com>
2024-09-06 01:58:23 +0630

    changes guides

--- src/screens/Guides.js
+++ src/screens/Guides.js
@@ -2,7 +2,9 @@ import React from 'react'
 
 const Guides = () => {
   return (
-    <div>Guides</div>
+    <div className='text-center'>
+      <h3>Guides are coming soon!</h3>
+    </div>
   )
 }
 
